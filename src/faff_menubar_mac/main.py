bf067e05b0a149c7f41e04a14391c56dfa2be871
Thomas Lant <tom@lant.uk>
2025-05-07 00:30:08 +0100

    Update to maintain compatibility

--- src/faff_menubar_mac/main.py
+++ src/faff_menubar_mac/main.py
@@ -10,10 +10,13 @@ class FaffMenubar(rumps.App):
     def __init__(self):
         super().__init__("🧠", title="ﬀ: Resting...")
         self.timer = rumps.Timer(self.update_title, 10)  # Update every 10 seconds
+        self.menu = [
+            "Stop"
+        ]
         self.timer.start()
 
     def update_title(self, _=None):
-        current_task = ws.get_log(ws.today()).active_timeline_entry()
+        current_task = ws.logs.get_log(ws.today()).active_timeline_entry()
         self.title = f"ﬀ: {current_task.activity.name}" if current_task else "ﬀ: Resting..."
 
 if __name__ == "__main__":
